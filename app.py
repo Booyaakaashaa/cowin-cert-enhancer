423ade710f5ed265fd264fffad973d8786c3c90b
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-06-14 03:38:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,137 @@
+import io
+
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
+import cv2 as cv
+import fitz
+import copy
+import numpy as np
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def upload():
+    return render_template("upload_cert.html")
+
+
+@app.route("/upload_pic", methods=["POST"])
+def upload_pdf():
+    if request.method == "POST":
+        f = request.files.get("certificate")
+        filename = secure_filename(f.filename)
+        pdf_file = fitz.open(filename, stream=f.read())
+        page = pdf_file[0]
+        zoom = 2.0
+        mat = fitz.Matrix(zoom, zoom)
+        pix = page.get_pixmap(matrix=mat)
+        img = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n)
+        img = np.ascontiguousarray(img[..., [2, 1, 0]])  # rgb to bgr
+
+        img[1200:1396, 70:684] = [239, 236, 232]  # Remove PM Photo
+
+        img[1394:1398, 72:684] = [221, 201, 184]  # Repair Line
+
+        img2 = cv.imread("flag.png", cv.IMREAD_UNCHANGED)  # Read Flag art
+        dim = (img.shape[1], img.shape[0])
+
+        img2_resize = cv.resize(img2, dim, interpolation=cv.INTER_AREA)  # Resizing Flag Art
+
+        output = copy.deepcopy(img)
+        output[:, :] = (img[:, :] / 255) * (img2_resize[:, :] / 255) * 255  # print flag art on pdf
+        upload_pdf.out = output
+
+        return render_template('upload_pic.html')
+
+
+@app.route("/thank_you", methods=["POST"])
+def upload_pic():
+    if request.method == "POST":
+        f = request.files.get("pic")
+
+        img3 = f.read()
+        img3 = np.frombuffer(img3, np.uint8)
+        img3 = cv.imdecode(img3, cv.IMREAD_COLOR)
+
+        img3 = cv.cvtColor(img3, cv.COLOR_BGR2GRAY)
+        img3 = cv.resize(img3, (272, 272), interpolation=cv.INTER_AREA) #read user image and resize
+        img_inv = 255 - img3
+        img_blur = cv.GaussianBlur(img_inv, (35, 35), 0)
+        output3 = cv.divide(img3, 255 - img_blur, scale=256)
+        output3 = cv.cvtColor(output3, cv.COLOR_GRAY2RGB)
+
+        output = upload_pdf.out
+
+        output[1124: 1396, 244: 516] = (output[1124: 1396, 244: 516] / 255) * (output3[:, :] / 255) * 255  # OVerlap user photo
+
+        img4 = cv.resize(cv.imread('stamp.png'), (630, 600), interpolation=cv.INTER_AREA)
+        output[950: 1550, 0: 630] = (output[950: 1550, 0: 630] / 255) * (img4[:, :] / 255) * 255
+
+        _, output_stream = cv.imencode('.png', output)
+        output_stream = output_stream.tobytes()
+
+        image = fitz.open("png", stream=output_stream)
+        pdf_bytes = image.convert_to_pdf()
+        image.close()
+
+        img_pdf = io.BytesIO(pdf_bytes)
+        return send_file(img_pdf, download_name="certificate.pdf", as_attachment=True, mimetype='application/pdf')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+"""#!/bin/python3
+import cv2 as cv
+import fitz
+import copy
+
+# Read Cert and convert to PNG
+f_name = "cert.pdf"
+pdf_file = fitz.open(f_name)
+page = pdf_file[0]
+pix = page.get_pixmap(alpha=False)
+pix.save("cert.png")
+
+img = cv.imread("cert.png")
+
+img[600:698, 35:342] = [239, 236, 232]  # Remove PM Photo
+
+img[697:699, 36:342] = [221, 201, 184]  # Repair Line
+
+img2 = cv.imread("flag.png", cv.IMREAD_UNCHANGED)  # Read Flag art
+dim = (img.shape[1], img.shape[0])
+
+img2_resize = cv.resize(img2, dim, interpolation=cv.INTER_AREA)  # Resizing Flag Art
+
+output = copy.deepcopy(img)
+output[:, :] = (img[:, :] / 255) * (img2_resize[:, :] / 255) * 255  # print flag art on pdf
+
+# Sketchify the user image
+# img3 = cv.imread('user.jpg', 0)
+img3 = cv.resize(cv.imread('user.jpg', 0), (136, 136), interpolation=cv.INTER_AREA)  # read user image and resize
+img_inv = 255 - img3
+img_blur = cv.GaussianBlur(img_inv, (21, 21), 0)
+output3 = cv.divide(img3, 255 - img_blur, scale=256)
+output3 = cv.cvtColor(output3, cv.COLOR_GRAY2RGB)
+
+output[562: 698, 122: 258] = (output[562: 698, 122: 258] / 255) * (output3[:, :] / 255) * 255  # OVerlap user photo
+
+# img4 = cv.imread('stamp.png')
+# Overlap stamp art on the image
+img4 = cv.resize(cv.imread('stamp.png'), (315, 300), interpolation=cv.INTER_AREA)
+output[475: 775, 0: 315] = (output[475: 775, 0: 315] / 255) * (img4[:, :] / 255) * 255
+cv.imwrite('output.png', output)
+
+# Create PDF from image
+img_pdf = fitz.open()
+image = fitz.open('output.png')
+shape = image[0].rect
+pdf_bytes = image.convert_to_pdf()
+image.close()
+img_page = fitz.open("pdf", pdf_bytes)
+page = img_pdf.new_page(width=shape.width, height=shape.height)
+page.show_pdf_page(shape, img_page, 0)
+img_pdf.save("cert_output.pdf")
+
+"""
---
+++ static/js/script_cert.js
@@ -0,0 +1,122 @@
+const script = document.createElement('script');
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+const container = document.querySelector(".container"),
+    dropArea = container.querySelector(".drop_area"),
+    form = container.querySelector("form"),
+    button = container.querySelector(".pdf_btn_container"),
+    btn = button.querySelector(".pdf_btn"),
+    input = container.querySelector("input"),
+    dropInfo = dropArea.querySelector(".drop_info"),
+    body = document.querySelector("body"),
+    icon = dropArea.querySelector(".icon"),
+    loading = dropArea.querySelector(".loading"),
+    orSpan = dropArea.querySelector(".or_span");
+
+let file;
+let ajaxData;
+
+container.onclick = () => {
+    input.click();
+}
+
+dropArea.addEventListener("dragover", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    dropArea.classList.add("drop_area--drag");
+    container.classList.add("container--drag");
+    orSpan.classList.add("or_span--drag");
+    dropInfo.classList.add("drop_info--drag");
+    btn.classList.add("pdf_btn--drag")
+    icon.classList.add("icon--drag");
+    button.classList.add("pdf_btn_container--drag");
+});
+
+["dragleave", "dragend"].forEach(action => {
+    dropArea.addEventListener(action, () => {
+        dropArea.classList.remove("drop_area--drag");
+        container.classList.remove("container--drag");
+        orSpan.classList.remove("or_span--drag");
+        dropInfo.classList.remove("drop_info--drag");
+        btn.classList.remove("pdf_btn--drag")
+        icon.classList.remove("icon--drag");
+        button.classList.remove("pdf_btn_container--drag");
+    });
+});
+
+dropArea.addEventListener("drop", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    dropArea.classList.add("drop_area--drag");
+    container.classList.add("container--drag");
+    orSpan.classList.add("or_span--drag");
+    dropInfo.classList.add("drop_info--drag");
+    btn.classList.add("pdf_btn--drag");
+    icon.classList.add("icon--drop");
+    loading.classList.remove("loading");
+    loading.classList.add("loading--drop");
+    button.classList.add("pdf_btn_container--drag");
+    /*reader.onload = ()=>{
+
+    };*/
+    file = e.dataTransfer.files;
+    //let $form = $('form');
+    //$form.trigger('submit');
+    console.log(file);
+    if (file.length > 1 || file.length === 0) {
+        alert("Multiple files selected!");
+        dropArea.classList.remove("drop_area--drag");
+        container.classList.remove("container--drag");
+        orSpan.classList.remove("or_span--drag");
+        dropInfo.classList.remove("drop_info--drag");
+        btn.classList.remove("pdf_btn--drag")
+        loading.classList.add("loading");
+        loading.classList.remove("loading--drop");
+        button.classList.remove("pdf_btn_container--drag");
+        icon.classList.remove("icon--drag");
+        icon.classList.remove("icon--drop");
+    } else {
+        file = file[0];
+        //console.log(file);
+        $(function(){
+
+            let $form = $("form");
+            //$form.trigger('submit');
+            ajaxData = new FormData();
+            console.log(1,file);
+            ajaxData.append($('input').attr('name'), file);
+            console.log(2,file);
+            console.log(ajaxData);
+            $.ajax({
+                url: $form.attr('action'),
+                type: $form.attr('method'),
+                data: ajaxData,
+                //dataType: 'json',
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function (data){
+                    console.log("Success");
+                },
+            });
+            //$form.trigger('submit');
+        });
+    }
+});
+
+document.getElementById("certificate").onchange = function () {
+    console.log("Le bhai main bhi chal gya!");
+    dropArea.classList.add("drop_area--drag");
+    container.classList.add("container--drag");
+    orSpan.classList.add("or_span--drag");
+    dropInfo.classList.add("drop_info--drag");
+    btn.classList.add("pdf_btn--drag");
+    icon.classList.add("icon--drop");
+    loading.classList.remove("loading");
+    loading.classList.add("loading--drop");
+    button.classList.add("pdf_btn_container--drag");
+    console.log(document.getElementById("certificate").value);
+    document.getElementsByTagName("form")[0].submit();
+}
