345f2740eb72a3a4fefc225ff802ab7cccfe5c06
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-06-14 14:10:12 +0530

    Parallel Processing

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import io
+import os
+import random
+import string
 from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
 import cv2 as cv
@@ -6,7 +8,14 @@ import fitz
 import copy
 import numpy as np
 
+UPLOAD_FOLDER = 'static/uploads/'
+DOWNLOAD_FOLDER = 'static/downloads/'
+
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+session_code = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
 
 
 @app.route("/")
@@ -19,7 +28,9 @@ def upload_pdf():
     if request.method == "POST":
         f = request.files.get("certificate")
         filename = secure_filename(f.filename)
-        pdf_file = fitz.open(filename, stream=f.read())
+        filename = session_code + filename
+        f.save(os.path.join(UPLOAD_FOLDER, filename))
+        pdf_file = fitz.open(os.path.join(UPLOAD_FOLDER, filename))
         page = pdf_file[0]
         zoom = 2.0
         mat = fitz.Matrix(zoom, zoom)
@@ -47,10 +58,10 @@ def upload_pdf():
 def upload_pic():
     if request.method == "POST":
         f = request.files.get("pic")
-
-        img3 = f.read()
-        img3 = np.frombuffer(img3, np.uint8)
-        img3 = cv.imdecode(img3, cv.IMREAD_COLOR)
+        filename = secure_filename(f.filename)
+        filename = session_code + filename
+        f.save(os.path.join(UPLOAD_FOLDER, filename))
+        img3 = cv.imread(os.path.join(UPLOAD_FOLDER, filename))
 
         img3 = cv.cvtColor(img3, cv.COLOR_BGR2GRAY)
         img3 = cv.resize(img3, (272, 272), interpolation=cv.INTER_AREA) #read user image and resize
@@ -61,20 +72,22 @@ def upload_pic():
 
         output = upload_pdf.out
 
-        output[1124: 1396, 244: 516] = (output[1124: 1396, 244: 516] / 255) * (output3[:, :] / 255) * 255  # OVerlap user photo
+        output[1124: 1396, 244: 516] = (output[1124: 1396, 244: 516] / 255) * (output3[:, :] / 255) * 255  # Overlap user photo
 
         img4 = cv.resize(cv.imread('stamp.png'), (630, 600), interpolation=cv.INTER_AREA)
         output[950: 1550, 0: 630] = (output[950: 1550, 0: 630] / 255) * (img4[:, :] / 255) * 255
-
-        _, output_stream = cv.imencode('.png', output)
-        output_stream = output_stream.tobytes()
-
-        image = fitz.open("png", stream=output_stream)
+        cv.imwrite(os.path.join(DOWNLOAD_FOLDER, session_code + 'output.png'), output)
+        img_pdf = fitz.open()
+        image = fitz.open(os.path.join(DOWNLOAD_FOLDER, session_code + 'output.png'))
+        shape = image[0].rect
         pdf_bytes = image.convert_to_pdf()
         image.close()
-
-        img_pdf = io.BytesIO(pdf_bytes)
-        return send_file(img_pdf, download_name="certificate.pdf", as_attachment=True, mimetype='application/pdf')
+        img_page = fitz.open("pdf", pdf_bytes)
+        page = img_pdf.new_page(width=shape.width, height=shape.height)
+        page.show_pdf_page(shape, img_page, 0)
+        img_pdf.save(os.path.join(DOWNLOAD_FOLDER, session_code + "cert_output.pdf"))
+        dir = os.path.join(DOWNLOAD_FOLDER, session_code + "cert_output.pdf")
+        return send_file(dir, download_name="certificate.pdf", as_attachment=True, mimetype='application/pdf')
 
 
 if __name__ == "__main__":
